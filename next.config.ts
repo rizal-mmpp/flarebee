5a81fc717214bd4de76fda4cdbbe69ab583cd278
Dono Zero <r124ltech@gmail.com>
2025-06-08 08:28:37 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'ptrio.j.erpnext.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
