5fd04140cacb95243e2a6e656d50bd0907b879a9
rizal-mmpp <rizal.mmpp07@gmail.com>
2025-07-09 09:30:48 +0000

    studio-5679535587:~/studio{v0.12-dockerize}$ docker-compose up Cannot c

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,6 +7,7 @@
     pkgs.nodejs_20      # For using npm/npx locally
     pkgs.docker         # Docker daemon and client
     pkgs.docker-compose # For running docker-compose commands
+    pkgs.zulu
   ];
 
   # Sets environment variables in the workspace.
@@ -14,4 +15,34 @@
 
   # To run the application, open a terminal and run `docker compose up`.
   # The web preview will be available on port 9002.
+  
+  # a firebase.json file is written into the user's directory
+  services.firebase.emulators = {
+    detect = true;
+    projectId = "demo-app";
+    services = ["auth" "firestore"];
+  };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+    ];
+    workspace = {
+      onCreate = {
+        default.openFiles = [
+          "src/app/page.tsx"
+        ];
+      };
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
+          manager = "web";
+        };
+      };
+    };
+  };
 }
